f4092af69f8a96088a942940a94079da677911b1
Andrei Taraskin <zrabadaber@gmail.com>
2023-01-04 17:40:53 +0300

    inited

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include <main.h>
+#include <icache.h>
+#include <gpio.h>
+
+extern void SystemClock_Config(void);
 
 void main(void) {
-  while (1) {
+  HAL_Init();
+  SystemClock_Config();
 
+  HAL_PWREx_DisableUCPDDeadBattery();
+  if (HAL_PWREx_ConfigSupply(PWR_SMPS_SUPPLY) != HAL_OK) {
+    Error_Handler();
   }
+
+  MX_ICACHE_Init();
+  MX_GPIO_Init();
+
+  while (1) {}
 }
